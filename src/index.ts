c49a8a00c0cc6236c00713e67aa82f1eb8899b8a
Dorian Peters <dpeters08@gmail.com>
2025-07-20 23:56:22 -0700

    Refactor index.ts to implement command-line options using Commander and parse date inputs; update package.json and pnpm-lock.yaml to include Commander and date-fns dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
-console.log("Hello");
+import { Command } from 'commander';
+import {parseISO, isValid} from 'date-fns';
+
+const program = new Command();
+
+program
+  .option('--date <string>', 'date to begin calculation')
+  .option('--courtDays <boolean>', 'whether to use court days, default is calendar days')
+  .option('-d, --days <numbers...>', 'number of days before or after the provided court date')
+
+
+program.parse(process.argv);
+const options = program.opts();
+
+const startingDate = parseISO(options.date) ?? new Date();
+const courtDays = options.courtDays ?? false;
+const daysArr = options.days;
+
+console.log(`date: ${startingDate}`);
+console.log(`courtDays: ${courtDays}`);
+console.log(`daysArr: ${daysArr}`);
